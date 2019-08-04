eea344acdc9a06f54236b6b2159825288aacfa9a
saimasharleen <saimasharleen0@gmail.com>
2019-08-04 23:34:25 +0600

    Create login.php

---
+++ app/login.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Userinfo extends Model
+{
+	protected $table      = 'userlogin';
+    protected $primaryKey = 'id';
+	public    $timestamps = false;
+}
