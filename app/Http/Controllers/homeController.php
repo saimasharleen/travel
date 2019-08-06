2eee4a1a2a7ae182276a3c1e879cca045a07af59
saimasharleen <saimasharleen0@gmail.com>
2019-08-06 19:33:32 +0600

    Create homeController.php

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\advertise;
+use App\notice;
+
+class homeController extends Controller
+{
+     public function index()
+    {  
+    	$ads = advertise::all();
+    	$notices = notice::all();
+         return view('index', compact(['ads','notices']));
+    }
+}
