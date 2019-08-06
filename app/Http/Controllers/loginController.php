90ff28e453fc456e8974ecbdfd38b96e3abee646
saimasharleen <saimasharleen0@gmail.com>
2019-08-06 19:32:38 +0600

    Create loginController.php

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Illuminate\Http\Request;
+
+class loginController extends Controller
+{
+       public function index(Request $request){
+
+    	return view('login');
+    }
+
+    public function verify(Request $request){
+    	
+    	$email = $request->email;
+    	$password = $request->input('password');
+     
+        $user = DB::table('userlogin')
+                ->where('email', $email)
+                ->where('password', $password)
+                ->first();
+
+		if($user != null){
+            if($user->usertype == "superadmin"){
+                $request->session()->put('logged', $user);
+    			return redirect()->route('superadmin.index');
+            }
+		}else{
+			
+			$request->session()->flash('message', 'Invalid email or password');
+			return redirect()->route('login', ['email'=>$email]);
+		}
+    	
+    }
+}
