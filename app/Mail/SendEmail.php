8aff4a6cc5104fd21772a4ce5805ac2d6d831a68
saimasharleen <saimasharleen0@gmail.com>
2019-08-06 19:31:46 +0600

    Create SendEmail.php

---
+++ app/Mail/SendEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    public $sub;
+    public $mes;
+    public function __construct($subject, $message)
+    {
+        $this->sub = $subject;
+        $this->mes = $message;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $e_subject = $this->sub;
+        $e_message = $this->mes;
+     //dd([$e_subject,$e_message]);
+        return $this->view('superadmin.pages.mail')->subject($e_subject);
+         // return $this->mes;
+
+    }
+}
