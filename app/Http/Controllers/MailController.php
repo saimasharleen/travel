6690267893948e4e530afd743a5b43162a43b0ef
saimasharleen <saimasharleen0@gmail.com>
2019-08-06 19:32:14 +0600

    Create MailController.php

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use Session;
+use App\Mail\SendEmail;
+class MailController extends Controller
+{
+    public function home(){
+        // dd($req->message);
+     return view('superadmin.pages.mail');
+    }
+
+    public function sendemail(Request $get){
+    	$this->validate($get,[
+    	  "email"   => "required",
+    	  "subject" => "required",
+    	  "message" => "required",
+    	]);
+    
+    	$email = $get->email;
+    	$subject = $get->subject;
+    	$message = $get->message;
+          // dd([$message,$subject]);
+    	Mail::to($email)->send(new SendEmail($subject, $message));
+    	Session::flash("Success");
+    	return back();
+    } 
+}
