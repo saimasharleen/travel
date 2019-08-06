1aeaeef27fbc9b02d31d24cd112031998ebf0c5c
saimasharleen <saimasharleen0@gmail.com>
2019-08-06 19:31:52 +0600

    Update login.php

--- app/login.php
+++ app/login.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Userinfo extends Model
+class login extends Model
 {
 	protected $table      = 'userlogin';
     protected $primaryKey = 'id';
