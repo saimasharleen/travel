160c515bf4754a699b3d3b000adb9a28dc42bdea
saimasharleen <saimasharleen0@gmail.com>
2019-08-07 16:21:07 +0600

    Update editprofile.blade.php

--- resources/views/superadmin/pages/editprofile.blade.php
+++ resources/views/superadmin/pages/editprofile.blade.php
@@ -1,13 +1,102 @@
 @extends('layouts.superadmin')
+@section('style')
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+@endsection
 @section('content')
 <div class="main-panel">
 	<div class="content-wrapper">
 		<div class="col-12">
-			<div class="card">
-				<div class="card-body">
-					<h4 class="card-title">    profile</h4>
-				</div>
-			</div>
+			<hr>
+<div class="container bootstrap snippet">
+    <div class="row">
+        <div class="col-sm-10"><h1>User name</h1></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+        </div>
+        <div class="col-sm-9">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="/superadmin/pages/profile">Profile</a></li>
+                <br>
+                <li><a href="/superadmin/pages/editprofile">Edit Profile</a></li>
+                <br>
+                <li><a href="/superadmin/pages/forgetpassword">Change Password</a></li>
+                <br>
+            </ul>
+                    <hr>
+                    <form class="form" action="" method="post">
+                    	@csrf
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="first_name"><h4>First name</h4></label>
+                                <input type="text" class="form-control" name="firstname" placeholder="first name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="last_name"><h4>Last name</h4></label>
+                                <input type="text" class="form-control" name="lastname" placeholder="last name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="Username"><h4>Username</h4></label>
+                                <input type="text" class="form-control" name="username" placeholder="username">
+                            </div>
+                        </div>
+                        
+
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="phone"><h4>Contactno</h4></label>
+                                <input type="text" class="form-control" name="contactno" placeholder="COntact No">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="city"><h4>City</h4></label>
+                                <input type="text" class="form-control" name="city" placeholder="City">
+                            </div>
+                        </div>
+                       
+
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="address"><h4>Address</h4></label>
+                                <input type="text" class="form-control" name="address" placeholder="Address">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="postalcode"><h4>Postal Code</h4></label>
+                                <input type="text" class="form-control" name="postalcode" placeholder="postalcode">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                <br>
+                                <button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div><!--/tab-pane-->
+            </div>
+        </div>
+  
 		</div>
 	</div>
 
@@ -33,5 +122,5 @@
 	@endsection
 
 	@section('title')
-	Edit Profile
+	Profile
 	@endsection
