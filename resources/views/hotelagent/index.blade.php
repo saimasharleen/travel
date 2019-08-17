96114a15ba08ef145b9b81b6e012b0282a0de24d
saimasharleen <saimasharleen0@gmail.com>
2019-08-17 13:14:57 +0600

    Create index.blade.php

---
+++ resources/views/hotelagent/index.blade.php
@@ -0,0 +1,354 @@
+@extends('layouts.hotelagent')
+@section('style')
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/animate.css">
+    
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/aos.css">
+
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/flaticon.css">
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/icomoon.css">
+    <link rel="stylesheet" href="{{asset('hotel')}}/css/style.css">
+@endsection
+
+@section('content')
+        <!-- partial -->
+        <div class="main-panel">
+           <section class="home-slider owl-carousel">
+      <div class="slider-item" style="background-image:url({{asset('hotel')}}/images/bg_1.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-12 ftco-animate text-center">
+          	<div class="text mb-5 pb-3">
+	            <h1 class="mb-3">Welcome To Deluxe</h1>
+	            <h2>Hotels &amp; Resorts</h2>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="slider-item" style="background-image:url({{asset('hotel')}}/images/bg_2.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-12 ftco-animate text-center">
+          	<div class="text mb-5 pb-3">
+	            <h1 class="mb-3">Enjoy A Luxury Experience</h1>
+	            <h2>Join With Us</h2>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-booking">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-lg-12">
+    				<form action="#" class="booking-form">
+	        		<div class="row">
+	        			<div class="col-md-3 d-flex">
+	        				<div class="form-group p-4 align-self-stretch d-flex align-items-end">
+	        					<div class="wrap">
+				    					<label for="#">Check-in Date</label>
+				    					<input type="text" class="form-control checkin_date" placeholder="Check-in date">
+			    					</div>
+			    				</div>
+	        			</div>
+	        			<div class="col-md-3 d-flex">
+	        				<div class="form-group p-4 align-self-stretch d-flex align-items-end">
+	        					<div class="wrap">
+				    					<label for="#">Check-out Date</label>
+				    					<input type="text" class="form-control checkout_date" placeholder="Check-out date">
+			    				</div>
+			    				</div>
+	        			</div>
+	        			<div class="col-md d-flex">
+	        				<div class="form-group p-4 align-self-stretch d-flex align-items-end">
+	        					<div class="wrap">
+			      					<label for="#">Room</label>
+			      					<div class="form-field">
+			        					<div class="select-wrap">
+			                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+			                    <select name="" id="" class="form-control">
+			                    	<option value="">Suite</option>
+			                      <option value="">Family Room</option>
+			                      <option value="">Deluxe Room</option>
+			                      <option value="">Classic Room</option>
+			                      <option value="">Superior Room</option>
+			                      <option value="">Luxury Room</option>
+			                    </select>
+			                  </div>
+				              </div>
+				            </div>
+		              </div>
+	        			</div>
+	        			<div class="col-md d-flex">
+	        				<div class="form-group p-4 align-self-stretch d-flex align-items-end">
+	        					<div class="wrap">
+			      					<label for="#">Customer</label>
+			      					<div class="form-field">
+			        					<div class="select-wrap">
+			                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+			                    <select name="" id="" class="form-control">
+			                    	<option value="">1 Adult</option>
+			                      <option value="">2 Adult</option>
+			                      <option value="">3 Adult</option>
+			                      <option value="">4 Adult</option>
+			                      <option value="">5 Adult</option>
+			                      <option value="">6 Adult</option>
+			                    </select>
+			                  </div>
+				              </div>
+				            </div>
+		              </div>
+	        			</div>
+	        			<div class="col-md d-flex">
+	        				<div class="form-group d-flex align-self-stretch">
+			              <input type="submit" value="Check Availability" class="btn btn-primary py-3 px-4 align-self-stretch">
+			            </div>
+	        			</div>
+	        		</div>
+	        	</form>
+	    		</div>
+    		</div>
+    	</div>
+    </section>
+
+
+    <section class="ftco-section ftc-no-pb ftc-no-pt">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url({{asset('hotel')}}/images/bg_2.jpg);">
+						<a href="https://vimeo.com/45830194" class="icon popup-vimeo d-flex justify-content-center align-items-center">
+							<span class="icon-play"></span>
+						</a>
+					</div>
+					<div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+	          <div class="heading-section heading-section-wo-line pt-md-5 pl-md-5 mb-5">
+	          	<div class="ml-md-0">
+		          	<span class="subheading">Welcome to Deluxe Hotel</span>
+		            <h2 class="mb-4">Welcome To Our Hotel</h2>
+	            </div>
+	          </div>
+	          <div class="pb-md-5">
+							<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+							<p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p>
+							<ul class="ftco-social d-flex">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="ftco-section">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center">
+              	<div class="icon d-flex align-items-center justify-content-center">
+              		<span class="flaticon-reception-bell"></span>
+              	</div>
+              </div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">25/7 Front Desk</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center">
+              	<div class="icon d-flex align-items-center justify-content-center">
+              		<span class="flaticon-serving-dish"></span>
+              	</div>
+              </div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Restaurant Bar</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-3 d-flex align-sel Searchf-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center">
+              	<div class="icon d-flex align-items-center justify-content-center">
+              		<span class="flaticon-car"></span>
+              	</div>
+              </div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Transfer Services</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center">
+              	<div class="icon d-flex align-items-center justify-content-center">
+              		<span class="flaticon-spa"></span>
+              	</div>
+              </div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Spa Suites</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section bg-light">
+    	<div class="container">
+				<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section text-center ftco-animate">
+            <h2 class="mb-4">Our Rooms</h2>
+          </div>
+        </div>    		
+    		<div class="row">
+    			<div class="col-sm col-md-6 col-lg-4 ftco-animate">
+    				<div class="room">
+    					<a href="rooms.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-1.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3 text-center">
+    						<h3 class="mb-3"><a href="rooms.blade.php">Suite Room</a></h3>
+    						<p><span class="price mr-2">$120.00</span> <span class="per">per night</span></p>
+    						<hr>
+    						<p class="pt-1"><a href="room-single.html" class="btn-custom">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg-4 ftco-animate">
+    				<div class="room">
+    					<a href="rooms.html" class="img d-flex justify-content-center align-items-center" style="background-image: url({{asset('hotel')}}/images/room-2.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3 text-center">
+    						<h3 class="mb-3"><a href="rooms.blade.php">Family Room</a></h3>
+    						<p><span class="price mr-2">$20.00</span> <span class="per">per night</span></p>
+    						<hr>
+    						<p class="pt-1"><a href="room-single.blade.php" class="btn-custom">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg-4 ftco-animate">
+    				<div class="room">
+    					<a href="rooms.blade.php" class="img d-flex justify-content-center align-items-center" style="background-image: url({{asset('hotel')}}/images/room-3.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3 text-center">
+    						<h3 class="mb-3"><a href="rooms.blade.php">Deluxe Room</a></h3>
+    						<p><span class="price mr-2">$150.00</span> <span class="per">per night</span></p>
+    						<hr>
+    						<p class="pt-1"><a href="room-single.blade.php" class="btn-custom">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg-4 ftco-animate">
+    				<div class="room">
+    					<a href="rooms.blade.php" class="img d-flex justify-content-center align-items-center" style="background-image: url({{asset('hotel')}}/images/room-4.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3 text-center">
+    						<h3 class="mb-3"><a href="rooms.blade.php">Classic Room</a></h3>
+    						<p><span class="price mr-2">$130.00</span> <span class="per">per night</span></p>
+    						<hr>
+    						<p class="pt-1"><a href="room-single.blade.php" class="btn-custom">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg-4 ftco-animate">
+    				<div class="room">
+    					<a href="rooms.html" class="img d-flex justify-content-center align-items-center" style="background-image: url({{asset('hotel')}}/images/room-5.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3 text-center">
+    						<h3 class="mb-3"><a href="rooms.blade.php">Superior Room</a></h3>
+    						<p><span class="price mr-2">$300.00</span> <span class="per">per night</span></p>
+    						<hr>
+    						<p class="pt-1"><a href="room-single.blade.php" class="btn-custom">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg-4 ftco-animate">
+    				<div class="room">
+    					<a href="rooms.blade.php" class="img d-flex justify-content-center align-items-center" style="background-image: url({{asset('hotel')}}/images/room-6.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3 text-center">
+    						<h3 class="mb-3"><a href="rooms.blade.php">Luxury Room</a></h3>
+    						<p><span class="price mr-2">$500.00</span> <span class="per">per night</span></p>
+    						<hr>
+    						<p class="pt-1"><a href="room-single.blade.php" class="btn-custom">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+          </div>
+  @endsection
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+  
+
+  @section('title')
+    Hotelagent Homepage
+  @endsection
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+@section('script')
+  <script src="{{asset('hotel')}}/js/jquery.min.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="{{asset('hotel')}}/js/popper.min.js"></script>
+  <script src="{{asset('hotel')}}/js/bootstrap.min.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery.easing.1.3.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery.waypoints.min.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery.stellar.min.js"></script>
+  <script src="{{asset('hotel')}}/js/owl.carousel.min.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery.magnific-popup.min.js"></script>
+  <script src="{{asset('hotel')}}/js/aos.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery.animateNumber.min.js"></script>
+  <script src="{{asset('hotel')}}/js/bootstrap-datepicker.js"></script>
+  <script src="{{asset('hotel')}}/js/jquery.timepicker.min.js"></script>
+  <script src="{{asset('hotel')}}/js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="{{asset('hotel')}}/js/google-map.js"></script>
+  <script src="{{asset('hotel')}}/js/main.js"></script>
+  @endsection
