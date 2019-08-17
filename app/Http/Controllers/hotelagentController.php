b8b3b2be909859c1691f218a2d8e76be1a0516a4
saimasharleen <saimasharleen0@gmail.com>
2019-08-18 01:06:25 +0600

    Update hotelagentController.php

--- app/Http/Controllers/hotelagentController.php
+++ app/Http/Controllers/hotelagentController.php
@@ -9,5 +9,33 @@ class hotelagentController extends Controller
 	public function index(){
 		return view('hotelagent.index');
 	}
+
+	public function advertisement()
+    {   $user = Userinfo::where('email',session('email'))->first();
+        return view('superadmin.pages.advertisement',compact('user'));
+    }
+    public function store(Request $request)
+    {
+		$this->validate($request, [
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    	]);
+
+    if ($request->hasFile('image')) {
+        $image = $request->file('image');
+        $name = time().'.'.$image->getClientOriginalExtension();
+        $destinationPath = public_path('/images');
+        $image->move($destinationPath, $name);
+        //$this->save();
+
+        $adv  = new advertise([
+            'image' => $name
+        ]);
+        $adv->save();
+
+         return back();
+        
+    }
+
+    }
     
 }
