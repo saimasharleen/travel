87e64c378b76a0b33a3346575aada83f6b637342
saimasharleen <saimasharleen0@gmail.com>
2019-08-19 20:31:07 +0600

    Create room.blade.php

---
+++ resources/views/hotelagent/room.blade.php
@@ -0,0 +1,210 @@
+@extends('layouts.hotelagent')
+@section('style')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+@endsection
+@section('content')
+ <section class="">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-9">
+            <div class="row">
+              <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+                <div class="room">
+                  <a href="rooms-single.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-1.jpg);">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                      <span class="icon-search2"></span>
+                    </div>
+                  </a>
+                  <div class="text p-3 text-center">
+                    <h3 class="mb-3"><a href="rooms-single.html">Suite Room</a></h3>
+                    <p><span class="price mr-2">$120.00</span> <span class="per">per night</span></p>
+                    <ul class="list">
+                      <li><span>Max:</span> 3 Persons</li>
+                      <li><span>Size:</span> 45 m2</li>
+                      <li><span>View:</span> Sea View</li>
+                      <li><span>Bed:</span> 1</li>
+                    </ul>
+                    <hr>
+                    <p class="pt-1"><a href="room-single.html" class="btn-custom">Book Now <span class="icon-long-arrow-right"></span></a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+                <div class="room">
+                  <a href="rooms-single.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-2.jpg);">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                      <span class="icon-search2"></span>
+                    </div>
+                  </a>
+                  <div class="text p-3 text-center">
+                    <h3 class="mb-3"><a href="rooms-single.html">Family Room</a></h3>
+                    <p><span class="price mr-2">$20.00</span> <span class="per">per night</span></p>
+                    <ul class="list">
+                      <li><span>Max:</span> 3 Persons</li>
+                      <li><span>Size:</span> 45 m2</li>
+                      <li><span>View:</span> Sea View</li>
+                      <li><span>Bed:</span> 1</li>
+                    </ul>
+                    <hr>
+                    <p class="pt-1"><a href="room-single.html" class="btn-custom">Book Now <span class="icon-long-arrow-right"></span></a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+                <div class="room">
+                  <a href="rooms-single.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-3.jpg);">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                      <span class="icon-search2"></span>
+                    </div>
+                  </a>
+                  <div class="text p-3 text-center">
+                    <h3 class="mb-3"><a href="rooms-single.html">Deluxe Room</a></h3>
+                    <p><span class="price mr-2">$150.00</span> <span class="per">per night</span></p>
+                    <ul class="list">
+                      <li><span>Max:</span> 5 Persons</li>
+                      <li><span>Size:</span> 45 m2</li>
+                      <li><span>View:</span> Sea View</li>
+                      <li><span>Bed:</span> 2</li>
+                    </ul>
+                    <hr>
+                    <p class="pt-1"><a href="room-single.html" class="btn-custom">Book Now <span class="icon-long-arrow-right"></span></a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+                <div class="room">
+                  <a href="rooms-single.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-4.jpg);">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                      <span class="icon-search2"></span>
+                    </div>
+                  </a>
+                  <div class="text p-3 text-center">
+                    <h3 class="mb-3"><a href="rooms-single.html">Classic Room</a></h3>
+                    <p><span class="price mr-2">$130.00</span> <span class="per">per night</span></p>
+                    <ul class="list">
+                      <li><span>Max:</span> 5 Persons</li>
+                      <li><span>Size:</span> 45 m2</li>
+                      <li><span>View:</span> Sea View</li>
+                      <li><span>Bed:</span> 2</li>
+                    </ul>
+                    <hr>
+                    <p class="pt-1"><a href="room-single.html" class="btn-custom">Book Now <span class="icon-long-arrow-right"></span></a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+                <div class="room">
+                  <a href="rooms-single.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-5.jpg);">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                      <span class="icon-search2"></span>
+                    </div>
+                  </a>
+                  <div class="text p-3 text-center">
+                    <h3 class="mb-3"><a href="rooms-single.html">Superior Room</a></h3>
+                    <p><span class="price mr-2">$300.00</span> <span class="per">per night</span></p>
+                    <ul class="list">
+                      <li><span>Max:</span> 6 Persons</li>
+                      <li><span>Size:</span> 45 m2</li>
+                      <li><span>View:</span> Sea View</li>
+                      <li><span>Bed:</span> 3</li>
+                    </ul>
+                    <hr>
+                    <p class="pt-1"><a href="room-single.html" class="btn-custom">Book Now <span class="icon-long-arrow-right"></span></a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+                <div class="room">
+                  <a href="rooms-single.html" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/room-6.jpg);">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                      <span class="icon-search2"></span>
+                    </div>
+                  </a>
+                  <div class="text p-3 text-center">
+                    <h3 class="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+                    <p><span class="price mr-2">$500.00</span> <span class="per">per night</span></p>
+                    <ul class="list">
+                      <li><span>Max:</span> 5 Persons</li>
+                      <li><span>Size:</span> 45 m2</li>
+                      <li><span>View:</span> Sea View</li>
+                      <li><span>Bed:</span> 2</li>
+                    </ul>
+                    <hr>
+                    <p class="pt-1"><a href="room-single.html" class="btn-custom">Book Now <span class="icon-long-arrow-right"></span></a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 sidebar">
+            <div class="sidebar-wrap bg-light ftco-animate">
+              <h3 class="heading mb-4">Advanced Search</h3>
+              <form action="#">
+                <div class="fields">
+                  <div class="form-group">
+                    <input type="text" id="check-in-date" class="form-control checkin_date" placeholder="Check In Date">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" id="check-out-date" class="form-control checkout_date" placeholder="Check Out Date">
+                  </div>
+                  <div class="form-group">
+                    <div class="select-wrap one-third">
+                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <select name="room" id="room" class="form-control">
+                            <option value="suite">Suite</option>
+                            <option value="family">Family Room</option>
+                            <option value="deluxe">Deluxe Room</option>
+                            <option value="classic">Classic Room</option>
+                            <option value="superior">Superior Room</option>
+                            <option value="luxury">Luxury Room</option>
+                          </select>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="select-wrap one-third">
+                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <select name="adult" id="adult" class="form-control">
+                        <option value="0">0 Adult</option>
+                        <option value="1">1 Adult</option>
+                        <option value="2">2 Adult</option>
+                        <option value="3">3 Adult</option>
+                        <option value="4">4 Adult</option>
+                        <option value="5">5 Adult</option>
+                        <option value="6">6 Adult</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="select-wrap one-third">
+                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <select name="children" id="children" class="form-control">
+                        <option value="0">0 Children</option>
+                        <option value="1">1 Children</option>
+                        <option value="2">2 Children</option>
+                        <option value="3">3 Children</option>
+                        <option value="4">4 Children</option>
+                        <option value="5">5 Children</option>
+                        <option value="6">6 Children</option>
+                      </select>
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">
+                    <input type="submit" value="Search" class="btn btn-primary py-3 px-5">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    
+   
+   
+    
+    
+  
+@endsection
