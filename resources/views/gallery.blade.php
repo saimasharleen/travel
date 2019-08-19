4cc97cd1cae586c30a36a3c5ac6b18811189c13b
saimasharleen <saimasharleen0@gmail.com>
2019-08-19 21:10:11 +0600

    Update gallery.blade.php

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -9,8 +9,12 @@
       </div>
       <div class="clear"></div>
       <div class="gallery">
-        <div class="grid_4"> <a href="images/big1.jpg" class="gal img_inner"><img src="images/page3_img1.jpg" alt=""></a> </div>
-        <div class="grid_4"> <a href="images/big2.jpg" class="gal img_inner"><img src="images/page3_img2.jpg" alt=""></a> </div>
+  
+              <ul>
+                @foreach ($gals as $gal)
+              
+               
+        <div class="grid_4"> <li><img src="/images/{{$gal->image}}" alt=""></li> </div>
         <div class="grid_4"> <a href="images/big3.jpg" class="gal img_inner"><img src="images/page3_img3.jpg" alt=""></a> </div>
         <div class="clear"></div>
         <div class="grid_4"> <a href="images/big4.jpg" class="gal img_inner"><img src="images/page3_img4.jpg" alt=""></a> </div>
@@ -19,7 +23,11 @@
         <div class="clear"></div>
         <div class="grid_4"> <a href="images/big7.jpg" class="gal img_inner"><img src="images/page3_img7.jpg" alt=""></a> </div>
         <div class="grid_4"> <a href="images/big8.jpg" class="gal img_inner"><img src="images/page3_img8.jpg" alt=""></a> </div>
-        <div class="grid_4"> <a href="images/big9.jpg" class="gal img_inner"><img src="images/page3_img9.jpg" alt=""></a> </div>
+        <div class="grid_4"> <a href="images/big9.jpg" class="gal img_inner"><img src="images/page3_img9.jpg" alt=""></a> </div> 
+            
+              </ul>
+         
+        @endforeach
       </div>
       <div class="clear"></div>
     </div>
